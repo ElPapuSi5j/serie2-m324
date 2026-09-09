99e174fbd1a7de5f543d51f95d541e1659d8fa3f
Barcella <marco.barcella10@gmail.com>
2026-09-09 13:44:59 +0200

    feat: completa funzionalità autenticazione

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ function avviaApplicazione() {
   console.log('Applicazione avviata');
 }
 
+function login(username, password) {
+  if (username && password) {
+    console.log('Accesso effettuato');
+    return true;
+  }
+  console.log('Credenziali mancanti');
+  return false;
+}
+
 avviaApplicazione();
